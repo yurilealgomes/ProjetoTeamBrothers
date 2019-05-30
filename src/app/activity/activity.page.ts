f6d3a2e18d895028f404b7712038e826d9059ce0
Murilo <murilonatan2013@hotmail.com>
2019-05-30 19:37:41 -0300

    Corrigido o Bug do Contador e pagina Activity feita com Dois Templates

--- src/app/activity/activity.page.ts
+++ src/app/activity/activity.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, ViewChild, ViewChildren, QueryList, forwardRef, ContentChild, ɵConsole, Injectable } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
 import { TrainingreleasePage } from '../trainingrelease/trainingrelease.page';
 import { RestPage } from '../rest/rest.page';
 import { Index } from '../singleton.service';
 import { SWITCH_TEMPLATE_REF_FACTORY__POST_R3__ } from '@angular/core/src/linker/template_ref';
+import { HomePage } from '../home/home.page';
+
 
 
 @Component({
@@ -22,6 +24,7 @@ export class ActivityPage implements OnInit {
   nextDay: any;
   template: any = 1;
   i: number = 0;
+  countsec: number = null;
 
   constructor(private route: Router,
     private router: ActivatedRoute,
@@ -29,20 +32,24 @@ export class ActivityPage implements OnInit {
 
 
   public switchTemplate() {
+    console.log("TAMANHO DO I ~~~~~~~~~~~~~~~~~~~~~~~~~> ", this.i);
+    console.log("TAMANHO DO I ~~~~~~~~~~~~~~~~~~~~~~~~~> ", this.dayData[this.i]);
     console.log('@@@@@@@@@@@@@@@@@@@@@@@@ ENTROU NO SWITCH TEMPLATE @@@@@@@@@@@@@@@@@@@@@@@@')
     if (this.template == 1) {
       this.defineTempo();
       this.template = 2;
       console.log('TROCOU TEMPLATE PRA 2 (REST)');
-    } else{
+    } else if (this.template == 2) {
       this.template = 1;
+      if(this.dayData[this.i].seconds != null )
+        this.countseconds();
       console.log('TROCOU TEMPLATE PRA 1 (ACTIVITY)')
     }
   }
 
   public switchExercise() {
     console.log('@@@@@@@@@@@@@@@@@@@@@@@@ ENTROU NO SWITCH EXERCISE @@@@@@@@@@@@@@@@@@@@@@@@');
-    this.i++;
+    this.i = this.i + 1;
     console.log('@@@@@@@@@@@@@@@@@@@@@@@@ CHAMOU O SWITCH TEMPLATE @@@@@@@@@@@@@@@@@@@@@@@@')
     this.switchTemplate();
   }
@@ -54,17 +61,32 @@ export class ActivityPage implements OnInit {
 
 
   public async defineTempo() {
-    this.timer = 30;
-    setInterval(() => {
-      if (this.timer != 0 && this.timer != null)
-        this.timer -= 1;
-      else {
-        clearInterval(this.timer);
+    this.timer = 3;
+    var interval = setInterval(() => {
+      console.log("CONTADOR =========== ", this.timer);
+      this.timer--;
+      if(this.timer < 0 ){
+        clearInterval(interval);
+        console.log('Ding!');
         this.switchTemplate();
-      }
+      };
+    }, 1000);
+  }
+
+  public async countseconds() {
+    this.countsec  = 3;
+    var interval = setInterval(() => {
+      console.log("CONTADOR =========== ", this.countsec);
+      this.countsec--;
+      if(this.countsec < 0 ){
+        clearInterval(interval);
+        console.log('Ding!');
+        this.switchExercise();
+      };
     }, 1000);
   }
 }
+
 
 
 
